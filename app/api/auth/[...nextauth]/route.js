1ffc184c14703ee79f2f2b46aa7f7e33fed6e84d
hackerx-in <ishwarmeena980@gmail.com>
2025-06-29 03:37:50 +0000

    update api

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,6 @@
 import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth";
 
-console.log("authOptions? ", typeof authOptions, authOptions?.providers?.length);
 
 const handler = NextAuth(authOptions);
 
